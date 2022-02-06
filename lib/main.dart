b74044e7279cf333d6a0b78c1d52508d967ab1e8
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-02-06 12:38:19 +0100

    added a function that return the quote template

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,29 @@ class _QuoteListState extends State<QuoteList> {
         text: 'The truth is rarely pure and never simple'),
   ];
 
+  Widget quoteTemplate(Quote quote) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,9 +62,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes
-            .map((quote) => Text('${quote.text} - ${quote.author}'))
-            .toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
